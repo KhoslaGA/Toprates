5d2d5720deeb89a5118c76a6d074f9bcbaa41f8f
Gautam Khosla <gautamkhosla75@gmail.com>
2026-04-14 18:19:49 -0400

    allow unsplash images
    mock blog posts reference images.unsplash.com; next/image blocks external hosts by default. added it to remotePatterns. will be dropped once sanity CDN serves real images.

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.sanity.io',
-      },
+      { protocol: 'https', hostname: 'cdn.sanity.io' },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
     ],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
